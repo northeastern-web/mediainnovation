0c5670a1835a42b6e9e1390bcad87991e5e99ddd
danzedek <danzedek@gmail.com>
2021-04-26 11:53:53 -0400

    Final Chuan and Conway

--- 2021/loveinthetimeofcovid/chuan/css/quiz.js
+++ 2021/loveinthetimeofcovid/chuan/css/quiz.js
@@ -1,78 +1,78 @@
-// Functions
-function buildQuiz(){
-    // variable to store the HTML output
-    const output = [];
-  
-    // for each question...
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
-  
-        // variable to store the list of possible answers
-        const answers = [];
-  
-        // and for each available answer...
-        for(letter in currentQuestion.answers){
-  
-          // ...add an HTML radio button
-          answers.push(
-            `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-        );
-      }
-    );
-  
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
-  }
-function showResults(){ const myQuestions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "c"
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+var questions = [
+  {
+      question: "When was OnlyFans created?",
+      a: "2012",
+      b: "2010",
+      c: "2016",
+      answer: "C"
     },
     {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
+      question: "When did OnlyFans seen a search increased due to the panademic?",
+      a: "March 2020",
+      b: "September 2020",
+      c: "January 2021",
+      answer: "A"
     },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
+  {
+      question: "True or False: Parasocial Relationships is a new concept",
+      a: "True",
+      b: "False",
+      c: "Unsure",
+      answer: "B"
+    },
+  
+  {
+      question: "True or False: Technology help influnced Parasocial Relationships",
+      a: "True",
+      b: "False",
+      c: "Unsure",
+      answer: "A"
     }
-  ]; }
+  ];
+	
+function get(x){
+  return document.getElementById(x);
+}
 
-// Variables
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [];
 
-// Kick things off
-buildQuiz();
+function renderQuestion(){
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>You got " +correct+" of "+questions.length+" questions correct </h2>";
+    get("test_status").innerHTML = "Quiz Completed";
+    pos = 0;
+    correct = 0;
+    return false;
+  }
 
-// Event listeners
-submitButton.addEventListener('click', showResults);
+  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+  
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+
+  test.innerHTML = "<h3>"+question+"</h3>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+
+function checkAnswer(){
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
+    }
+  }
+
+  if(choice == questions[pos].answer){
+    correct++;
+  }  
+  pos++;
+  renderQuestion();
+}
+
+window.addEventListener("load", renderQuestion);
